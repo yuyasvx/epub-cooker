fd8c31d35582c7903fcef3eb07ac8f1c8a7e60d7
yuyasvx <yuyasvx@gmail.com>
2024-11-12 01:19:15 +0900

    EC-78 テストコード追加＋

--- src/domain/logic/book-context/ArtworkProcessor.ts
+++ src/domain/logic/book-context/ArtworkProcessor.ts
@@ -1,3 +1,6 @@
+import { MessageLevelType } from "../../../cli/enums/MessageLevelType";
+import { cliMessage } from "../../../cli/logic/CliMessageDispatcher";
+import { newMessage } from "../../../cli/value/Message";
 import { BookContext } from "../../../domain/data/BookContext";
 
 export function processArtwork(context: BookContext, coverImagePath: string | undefined) {
@@ -22,9 +25,9 @@ export function processArtwork(context: BookContext, coverImagePath: string | un
 
 function handleNoCoverImage(filePath: string | undefined) {
   if (filePath != null) {
-    console.log(`カバー画像ファイル(${filePath})が見つかりませんでした。`);
+    cliMessage.put(newMessage(`カバー画像ファイル(${filePath})が見つかりませんでした。`, MessageLevelType.WARN));
   }
   if (filePath == null) {
-    console.log("カバー画像が定義されていません。");
+    cliMessage.put(newMessage("カバー画像が定義されていません。", MessageLevelType.WARN));
   }
 }
